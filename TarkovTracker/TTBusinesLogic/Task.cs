e31453154570f294137aee9d7d2a84e8b75d505e
Esor555 <Stijnh1905@gmail.com>
2025-03-28 10:41:51 +0100

    making the project in vc added basic classes
    added a razor pages project and a class library

---
+++ TarkovTracker/TTBusinesLogic/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTBusinesLogic
+{
+    public class Task
+    {
+        private int Id { get; set; }
+        private string Name {get; set; }
+
+    }
+}
